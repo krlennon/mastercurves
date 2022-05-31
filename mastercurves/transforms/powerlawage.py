5e0bef9947edb7f50f364293a747ee9cfb8846cf
Kyle Lennon <krlennon@mit.edu>
2022-05-31 11:47:10 -0400

    add copyright/license to each file

--- mastercurves/transforms/powerlawage.py
+++ mastercurves/transforms/powerlawage.py
@@ -1,3 +1,21 @@
+# Copyright (c) 2022 Kyle R. Lennon (kyle.lennon08@gmail.com).
+#
+# This file is part of mastercurves.
+#
+# mastercurves is free software: you can redistribute it and/or modify it under the terms of the
+# GNU General Public License as published by the Free Software Foundation, either version 3
+# of the License, or (at your option) any later version.
+#
+# mastercurves is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+# without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+# See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with mastercurves.
+# If not, see <https://www.gnu.org/licenses/>.
+#
+# Please cite "A Data-Driven Method for Automated Data Superposition with Applications
+# in Soft Matter Science" (https://arxiv.org/abs/2204.09521) if you use any part of the code.
+
 import numpy as np
 
 class PowerLawAge:
